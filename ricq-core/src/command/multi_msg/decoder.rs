46445c589a45cbb149f35aa0cafc9df9e9cdafe4
BlueGlassBlock <blueglassblock@outlook.com>
2023-04-12 13:22:16 +0800

    feat: initial support currently only reqwest, will add hyper as 1st party support

--- ricq-core/src/command/multi_msg/decoder.rs
+++ ricq-core/src/command/multi_msg/decoder.rs
@@ -1,7 +1,19 @@
-use bytes::Bytes;
+use std::{collections::HashMap, io::Write};
 
-use crate::{pb, RQError, RQResult};
+use bytes::{Buf, Bytes};
+
+use crate::{msg::MessageChain, pb, RQError, RQResult};
+use flate2::write::GzDecoder;
 use prost::Message;
+
+use super::{ForwardMessage, ForwardNode, MessageNode};
+
+fn find_res_id(template: &str) -> Option<&str> {
+    template
+        .rsplit_once("m_resid=\"")
+        .and_then(|v| v.1.split_once("\""))
+        .map(|v| v.0)
+}
 
 impl super::super::super::Engine {
     pub fn decode_multi_msg_apply_down_resp(
@@ -12,6 +24,95 @@ impl super::super::super::Engine {
             .multimsg_applydown_rsp
             .pop()
             .ok_or(RQError::EmptyField("multimsg_applydown_rsp"))
+    }
+
+    pub fn decode_multi_msg_transmit(
+        &self,
+        mut payload: Bytes,
+        msg_key: &[u8],
+    ) -> RQResult<HashMap<String, pb::msg::PbMultiMsgItem>> {
+        if payload.get_u8() != 40 {
+            Err(RQError::Decode(format!(
+                "Unexpected body data: {payload:?}"
+            )))?
+        }
+        let msg_head_len = payload.get_i32();
+        let data_len = payload.get_i32();
+        payload.advance(msg_head_len as usize); // Skip IM msg head
+        let decrypted = crate::crypto::qqtea_decrypt(&payload[0..data_len as usize], msg_key);
+        let compressed = pb::longmsg::LongRspBody::decode(&*decrypted)?
+            .msg_down_rsp
+            .pop()
+            .ok_or(RQError::EmptyField("msg_down_rsp"))?
+            .msg_content;
+        let mut decoder = GzDecoder::new(vec![]);
+        decoder.write_all(&compressed)?;
+        Ok(
+            pb::msg::PbMultiMsgTransmit::decode(Bytes::from(decoder.finish()?))?
+                .pb_item_list
+                .into_iter()
+                .map(|item| (item.file_name.clone().unwrap_or_default(), item))
+                .collect(),
+        )
+    }
+
+    pub fn decode_forward_message(
+        &self,
+        name: &str,
+        items: &HashMap<String, pb::msg::PbMultiMsgItem>,
+    ) -> RQResult<Vec<ForwardMessage>> {
+        let item = match items.get(name) {
+            Some(item) => item.clone(),
+            None => Err(RQError::Other(format!(
+                "{name} does not exist in MultiMsgItem map"
+            )))?,
+        };
+        let msgs = item.buffer.unwrap_or_default().msg;
+        let mut nodes: Vec<ForwardMessage> = Vec::with_capacity(msgs.len());
+        'iter_main: for msg in msgs.into_iter() {
+            let head = msg.head.unwrap_or_default();
+            let sender_name: String;
+            if let Some(ref group_info) = head.group_info
+            && head.msg_type() == 82 {
+                sender_name = String::from_utf8(group_info.group_card().into())?;
+            } else {
+                sender_name = head.from_nick().to_string();
+            }
+            let elements = MessageChain::from(
+                msg.body
+                    .unwrap_or_default()
+                    .rich_text
+                    .unwrap_or_default()
+                    .elems,
+            );
+            for elem in elements.0.iter() {
+                match elem {
+                    crate::pb::msg::elem::Elem::RichMsg(rich) => {
+                        if rich.service_id() == 35 {
+                            let rich = crate::msg::elem::RichMsg::from(rich.clone());
+                            let res_id = find_res_id(&rich.template1)
+                                .ok_or_else(|| RQError::EmptyField("m_resid"))?;
+                            let sub_nodes = self.decode_forward_message(res_id, items)?;
+                            nodes.push(ForwardMessage::Forward(ForwardNode {
+                                sender_id: head.from_uin(),
+                                time: head.msg_time(),
+                                sender_name,
+                                nodes: sub_nodes,
+                            }));
+                            continue 'iter_main;
+                        }
+                    }
+                    _ => {}
+                }
+            }
+            nodes.push(ForwardMessage::Message(MessageNode {
+                sender_id: head.from_uin(),
+                time: head.msg_time(),
+                sender_name,
+                elements,
+            }))
+        }
+        Ok(nodes)
     }
 
     pub fn decode_multi_msg_apply_up_resp(
---
+++ ricq/src/ext/http.rs
@@ -0,0 +1,91 @@
+use ricq_core::error::RQError;
+use std::collections::HashMap;
+
+pub(crate) fn make_headers(
+    version: impl Into<String>,
+    cookie: impl Into<String>,
+) -> HashMap<String, String> {
+    let version = version.into();
+    let cookie = cookie.into();
+    let mut headers: HashMap<String, String> = HashMap::new();
+    headers.insert("User-Agent".into(), format!("QQ/{version} CFNetwork/1126"));
+    headers.insert("Net-Type".into(), "Wifi".into());
+    if !cookie.is_empty() {
+        headers.insert("Cookie".into(), cookie);
+    }
+    headers
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum HttpMethod {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+}
+
+#[async_trait::async_trait]
+pub trait HttpClient {
+    async fn make_request(
+        &mut self,
+        method: HttpMethod,
+        url: String,
+        header: &HashMap<String, String>,
+        body: bytes::Bytes,
+    ) -> Result<bytes::Bytes, RQError>;
+}
+
+#[cfg(feature = "http-reqwest")]
+mod http_reqwest {
+    use super::*;
+    use reqwest::header::HeaderMap;
+    use reqwest::Error as ReqErr;
+    use reqwest::{Client, Method as ReqMethod};
+    use std::io::{Error as IOErr, ErrorKind as IOErrKind};
+
+    impl From<HttpMethod> for ReqMethod {
+        fn from(value: HttpMethod) -> Self {
+            match value {
+                HttpMethod::GET => ReqMethod::GET,
+                HttpMethod::POST => ReqMethod::POST,
+                HttpMethod::PUT => ReqMethod::PUT,
+                HttpMethod::DELETE => ReqMethod::DELETE,
+            }
+        }
+    }
+
+    fn map_req_err(value: ReqErr) -> RQError {
+        if value.is_builder() {
+            RQError::Other(value.to_string())
+        } else {
+            RQError::IO(IOErr::new(IOErrKind::Other, value))
+        }
+    }
+
+    #[async_trait::async_trait]
+    impl HttpClient for Client {
+        async fn make_request(
+            &mut self,
+            method: HttpMethod,
+            url: String,
+            headers: &HashMap<String, String>,
+            body: bytes::Bytes,
+        ) -> Result<bytes::Bytes, RQError> {
+            let headers: HeaderMap =
+                HeaderMap::try_from(headers).map_err(|e| RQError::Other(e.to_string()))?;
+            let req = self
+                .request(method.into(), url)
+                .headers(headers)
+                .body(body)
+                .build()
+                .map_err(map_req_err)?;
+            Ok(self
+                .execute(req)
+                .await
+                .map_err(map_req_err)?
+                .bytes()
+                .await
+                .map_err(map_req_err)?)
+        }
+    }
+}
